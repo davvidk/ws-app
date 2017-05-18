c658d50548658901135621bb4473efd7b062b931
Dawid Kostrzycki <dawid.kostrzycki@cofinpro.de>
2017-05-18 11:43:27 +0200

    unsubscription of book data service

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Book } from './../shared/book';
 import { BookDataService } from './../shared/book-data.service';
+import { Subscription } from 'rxjs/subscription'
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
   constructor(private bookData: BookDataService) { }
 
   public books: Book[];
-  
+  private subscription: Subscription;
+
   ngOnInit() {
-      this.bookData.getBooks().subscribe(books => this.books = books);
+    this.subscription = this.bookData.getBooks().subscribe(books => this.books = books);
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    console.log("BookListComponent unsubscribed")
+  }
+
 }
